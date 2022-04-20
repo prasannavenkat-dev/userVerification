b2d6146e12555ef531642a61273c6ed8cae2098c
unknown <prasannavenkat1998@yahoo.com>
2022-04-20 23:00:29 +0530

    Implemented Login Api

--- backend/app.js
+++ backend/app.js
@@ -14,38 +14,41 @@ mongoose.connect(process.env.DB_URL);
 const PORT = process.env.port || 3000;
 
 
+const userSchema = new mongoose.Schema({
+    fullName:{
+        type:String,
+        minlength:2,
+        maxlength:20,
+        required:true
+    },
+    age:{
+        type:Number,
+        required:true
+    },
+    gender:{
+        type:String,
+        required:true
+    },
+    mail:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const User = mongoose.model("User",userSchema);
+
+
 app.get("/",function(req,res){
     res.send("IT WORKS!")
 })
 //Register User
 app.post('/register',function(req,res){
     const {fullName,age,gender,mail,password} = req.body;
-    const userSchema = new mongoose.Schema({
-        fullName:{
-            type:String,
-            minlength:2,
-            maxlength:20,
-            required:true
-        },
-        age:{
-            type:Number,
-            required:true
-        },
-        gender:{
-            type:String,
-            required:true
-        },
-        mail:{
-            type:String,
-            required:true
-        },
-        password:{
-            type:String,
-            required:true
-        }
-    })
-
-    const User = mongoose.model("User",userSchema);
+   
 
     const userData = new User({fullName,age,gender,mail,password});
 
@@ -61,7 +64,37 @@ app.post('/register',function(req,res){
     });
 });
 
+//Login User
 
+app.post("/login",function(req,res){
+
+    const {mail,password} = req.body;
+
+    const user = User.findOne({mail},function(err,user){
+        if(err){
+            console.log(err);
+        }
+        else{
+            //Checks user if Existed
+
+            if(user){
+                //Checks Password
+                if(password == user.password){
+                    res.send("Login Successfully!!");
+                }
+                else{
+                    res.send("Invalid Login!!")
+                }
+                
+            }
+            else{
+                res.send("User Not existed!!")
+            }
+
+        }
+    })
+
+})
 
 
 
